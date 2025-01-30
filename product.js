ff056f0f31d4ccf86309fa037f53ca6a07e30457
Astridkc <asch0001@stud.kea.dk>
2025-01-30 12:37:14 +0100

    product js

---
+++ product.js
@@ -0,0 +1,28 @@
+let productContainer = document.querySelector(".productContainer");
+let productId = 1164;
+fetch(`https://kea-alt-del.dk/t7/api/products/${productId}`)
+  .then((response) => response.json())
+  .then((data) => {
+    productContainer.innerHTML = `
+    <div class="product_img">
+                <img src="https://kea-alt-del.dk/t7/images/webp/640/${productId}.webp" alt="tshirt">
+            </div>
+            <div class="product_box">
+                <h2>Product information</h2>
+                <h3>Model name</h3>
+                <p>${data.productdisplayname}</p>
+                <h3>Color</h3>
+                <p>${data.season}</p>
+                <h3>Inventory number</h3>
+                <p>${data.articletype}</p>
+                <h2>Nike</h2>
+                <p>Nike, creating experiences for todays athlete</p>
+            </div>
+            <div class="kurv">
+                <h2>Sahara Team India Fanwear Round Neck Jersey</h2>
+                <p>NIKE - T-shirts</p>
+                <p>Choose a size <button>S</button></p>
+                <button>Add to basket</button>
+
+            </div>`;
+  });
